25dd7867f5c8a6553032e018d14d1a44486f0042
user <ojemehprogress@gmail.com>
2020-07-30 20:41:04 +0100

    aa

---
+++ footballassets/js/football.js
@@ -0,0 +1,9 @@
+ // $(window).scroll(function(){
+ //        if ($(window).scrollTop() == $(document).height()-$(window).height()){
+ //           $.ajax({
+ //              url: "../../footbal.html",
+ //              success: function (data) { $('body').append(data); },
+ //              dataType: 'html'
+ //           });
+ //        }
+ //    });
